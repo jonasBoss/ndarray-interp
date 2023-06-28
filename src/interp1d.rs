ee110843537be15cbd5057ffbbd6bc60dc65ae91
Jonas Bosse <jonas.bosse@posteo.de>
2023-06-28 15:30:12 +0200

    basic tests for interp1d

--- src/interp1d.rs
+++ src/interp1d.rs
@@ -1,6 +1,6 @@
 use std::ops::{Index, Sub};
 
-use ndarray::{ArrayBase, Data, Ix1, RawData};
+use ndarray::{Array, ArrayBase, Data, Ix1, RawData};
 use num_traits::{Float, Num, NumCast, PrimInt, ToPrimitive};
 
 #[derive(Debug)]
@@ -25,19 +25,23 @@ where
     S: RawData<Elem = T> + Data,
     T: Float,
 {
-    pub fn interp(&self, x: &T) -> T {
+    pub fn interp(&self, x: T) -> T {
         match self.strategy {
             InterpolationStrategy::Linear => self.linear(x),
         }
     }
 
-    fn linear(&self, x: &T) -> T {
+    pub fn interp_array<Dim>(&self, x: ArrayBase<S, Dim>) -> Array<T, Dim> {
+        todo!()
+    }
+
+    fn linear(&self, x: T) -> T {
         let idx = self.get_left_index(x);
         let (x1, y1) = self.get_point(idx);
         let (x2, y2) = self.get_point(idx + 1);
         let m = (y2 - y1) / (x2 - x1);
         let b = y1 - m * x1;
-        m * *x + b
+        m * x + b
     }
 
     /// get x,y coordinate at given index
@@ -56,13 +60,13 @@ where
     }
 
     /// the index of known value left of, or at x
-    fn get_left_index(&self, x: &T) -> usize {
+    fn get_left_index(&self, x: T) -> usize {
         if let Some(xs) = &self.x {
             // do bisection
             let mut range = (0usize, xs.len());
             while range.0 < range.1 + 1 {
                 let mid_idx = (range.1 - range.0) / 2 + range.0;
-                let mid_x = xs.get(mid_idx).unwrap_or_else(|| unreachable!());
+                let mid_x = *xs.get(mid_idx).unwrap_or_else(|| unreachable!());
                 if mid_x == x {
                     return mid_idx;
                 }
@@ -74,7 +78,32 @@ where
             }
             range.0
         } else {
-            x.floor().to_usize().unwrap()
+            match x.ceil().to_usize().unwrap() {
+                0 => 0,     // avoid out of bounds left
+                x => x - 1, // avoid out of bounds right
+            }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use ndarray::array;
+
+    use super::Interp1D;
+    use super::InterpolationStrategy;
+
+    #[test]
+    fn interp_y_only() {
+        let interp = Interp1D {
+            x: None,
+            y: array![1.0, 2.0, 3.0, 4.0, 5.0, 5.0, 4.0, 3.0, 2.0, 1.0],
+            strategy: InterpolationStrategy::Linear,
+        };
+        assert_eq!(interp.interp(0.0), 1.0);
+        assert_eq!(interp.interp(9.0), 1.0);
+        assert_eq!(interp.interp(4.5), 5.0);
+        assert_eq!(interp.interp(0.5), 1.5);
+        assert_eq!(interp.interp(8.75), 1.25);
     }
 }
