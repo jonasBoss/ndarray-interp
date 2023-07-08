79d878c1c17110ed87dfdeb5b6cfa0adbe450992
Jonas Bosse <jonas.bosse@posteo.de>
2023-07-08 11:22:51 +0200

    remove default template stuff

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,3 @@ mod vector_extensions;
 pub use interp1d::*;
 pub use interp2d::*;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
