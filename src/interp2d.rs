823243d54e8d11c6a203fe5b6197177ded7c0c6c
Jonas Bosse <jonas.bosse@posteo.de>
2023-07-18 00:01:40 +0200

    impl Interp2DBuilder::build()

--- src/interp2d.rs
+++ src/interp2d.rs
@@ -6,7 +6,10 @@ use std::{
 use ndarray::{Array1, ArrayBase, Data, Dimension, Ix1, OwnedRepr};
 use num_traits::{Num, NumCast, cast};
 
-use crate::BuilderError;
+use crate::{
+    vector_extensions::{Monotonic, VectorExtensions},
+    BuilderError, InterpolateError,
+};
 
 use self::strategies::{Biliniar, StrategyBuilder};
 
@@ -80,6 +83,7 @@ where
     Sx: Data<Elem = Sd::Elem>,
     Sy: Data<Elem = Sd::Elem>,
     D: Dimension, 
+    Strat: StrategyBuilder<Sd, Sx, Sy, D>
 {
     pub fn strategy<NewStrat: StrategyBuilder<Sd, Sx, Sy, D>>(self, strategy: NewStrat) -> Interp2DBuilder<Sd, Sx, Sy, D, NewStrat>{
         let Interp2DBuilder { x, y, data, .. } = self;
@@ -96,7 +100,33 @@ where
         Interp2DBuilder { x, y, data, strategy }
     }
 
-    pub fn build(self) -> Result<Interp2D<Sd, Sx, Sy, D, Strat>, BuilderError>{
-        todo!()
+    pub fn build(self) -> Result<Interp2D<Sd, Sx, Sy, D, Strat::FinishedStrat>, BuilderError>{
+        use self::Monotonic::*;
+        use BuilderError::*;
+        let Interp2DBuilder { x, y, data, strategy: stratgy_builder } = self;
+        if data.ndim() < 2{
+            return Err(DimensionError("data dimension needs to be at least 2".into()));
+        }
+        if data.shape()[0] < Strat::MINIMUM_DATA_LENGHT {
+            return Err(NotEnoughData(format!("The 0-dimension has not enough data for the chosen interpolation strategy. Provided: {}, Reqired: {}", data.shape()[0], Strat::MINIMUM_DATA_LENGHT)));
+        }
+        if data.shape()[1] < Strat::MINIMUM_DATA_LENGHT {
+            return Err(NotEnoughData(format!("The 1-dimension has not enough data for the chosen interpolation strategy. Provided: {}, Reqired: {}", data.shape()[1], Strat::MINIMUM_DATA_LENGHT)));
+        }
+        if !matches!(x.monotonic_prop(), Rising { strict: true }){
+            return  Err(Monotonic("The x-axis needs to be strictly monotonic rising".into()));
+        }
+        if !matches!(y.monotonic_prop(), Rising { strict: true }){
+            return Err(Monotonic("The y-axis needs to be strictly monotonic rising".into()));
+        }
+        if x.len() != data.shape()[0] {
+            return Err(AxisLenght(format!("Lenghts of x-axis and data-0-axis need to match. Got x: {}, data-0: {}", x.len(), data.shape()[0])));
+        }  
+        if y.len() != data.shape()[1]{
+            return Err(AxisLenght(format!("Lenghts of y-axis and data-1-axis need to match. Got y: {}, data-1: {}", y.len(), data.shape()[1])));
+        }
+        
+        let strategy = stratgy_builder.build(&x, &y, &data)?;
+        Ok(Interp2D { x, y, data, strategy })
     }
 }
