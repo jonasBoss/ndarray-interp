87d3484dd1d092c8d0f5ccef718e25c03efd35da
Jonas Bosse <jonas.bosse@posteo.de>
2024-02-12 11:30:03 +0100

    mark new_unchecked methods as unsafe

--- src/interp1d/mod.rs
+++ src/interp1d/mod.rs
@@ -359,7 +359,7 @@ where
     ///  - `x` is stricktly monotonic rising
     ///  - `data.shape()[0] == x.len()`
     ///  - the `strategy` is porperly initialized with the data
-    pub fn new_unchecked(x: ArrayBase<Sx, Ix1>, data: ArrayBase<Sd, D>, strategy: Strat) -> Self {
+    pub unsafe fn new_unchecked(x: ArrayBase<Sx, Ix1>, data: ArrayBase<Sd, D>, strategy: Strat) -> Self {
         Interp1D { x, data, strategy }
     }
 
